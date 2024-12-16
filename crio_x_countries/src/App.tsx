17813b43ac66c376c800e71e8ad5aff2ddbb60e4
Ayush Wardhan (Second Account) <45749740+xshayu@users.noreply.github.com>
2024-12-16 22:20:53 +0000

    [FEAT] implemented other assignment on top of this one

--- crio_x_countries/src/App.tsx
+++ crio_x_countries/src/App.tsx
@@ -2,51 +2,73 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 type FlagItem = {
-  name: string;
-  flag: string;
+  name: {
+    common: string;
+    official: string;
+  };
+  flags: {
+    png: string;
+  }
 };
 
-function CountryFlag({ name, flag }: FlagItem) {
+function CountryFlag({ name, flags }: FlagItem) {
   return (
-    <div className="flag">
-      <img src={flag} alt={`Flag of ${name}`} />
-      <p>{name}</p>
+    <div className="countryCard">
+      <img src={flags.png} alt={`Flag of ${name}`} />
+      <p>{name.common}</p>
     </div>
   );
 }
 
+let countryData = [] as FlagItem[];
+
 function App() {
   const [countries, setCountries] = useState<FlagItem[]>([]);
-  const [error, setError] = useState<string>('');
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     const fetchCountries = async () => {
       try {
-        const response = await fetch('https://xcountries-backend.azurewebsites.net/all');
+        const response = await fetch('https://restcountries.com/v3.1/all');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
+        countryData = data;
         setCountries(data);
       } catch (error) {
         const errorMessage = `Error fetching data: ${error instanceof Error ? error.message : 'Unknown error'}`;
         console.error(errorMessage);
-        setError(errorMessage);
       }
     };
 
     fetchCountries();
   }, []);
 
+  useEffect(() => {
+    setCountries(filter?.length > 0 ?
+      countryData.filter((country) => {
+        let { common, official } = country.name;
+        common = common.toLowerCase();
+        official = official.toLocaleLowerCase();
+        const thisFilter = filter.toLowerCase();
+        return common.includes(thisFilter) || official.includes(thisFilter);
+      }) :
+      countryData
+    ) 
+  }, [filter]);
+
   return (
     <div className="container">
-      {error && <p className="error">{error}</p>}
+      <nav>
+        <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </nav>
       <div className="grid">
         {countries.map((country, cIdx) => ( // Some countries are apparently repeated, idk if they are meant to not be included
           <CountryFlag
             key={country.name + '-' + cIdx}
             name={country.name}
-            flag={country.flag}
+            flags={country.flags}
           />
         ))}
       </div>
